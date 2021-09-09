925e42aa10f1638386c03b39cedee0b05f110bb5
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-09-09 10:27:46 +0200

    add information Array

---
+++ Javascript/informationArrays.js
@@ -0,0 +1,21 @@
+const educations = [
+	{
+		Id: 01,
+		Name: "HBO - Front-End Developer",
+		Location: "WINC Academy | 2020 - 2021",
+		Definition:
+			"Omscholing tot Front-End Developer. Behandelde onderwerpen: HTML, CSS/SCSS, Programmeren met Flexbox en Grid, Programmeren op de BEM & Sass manier, (vanilla) Javascript, Git & Github, Data ophalen via Postman en Javascript, TDD met Jest, React."
+	},
+	{
+		Id: 02,
+		Name: "BBL niveau 2 - Groenvoorziening",
+		Location: "Groenhorst College Emmeloord | 2000 - 2002",
+		Definition: null
+	},
+	{
+		Id: 03,
+		Name: "VMBO niveau 2",
+		Location: "Groenhorst College Emmeloord | 1996 - 2000",
+		Definition: null
+	}
+];
