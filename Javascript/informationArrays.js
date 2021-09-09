e9d4e325a1c7a2afc06d4cd8946cfc379b2dc64d
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-09-09 10:55:55 +0200

    add input to information Arrays

--- Javascript/informationArrays.js
+++ Javascript/informationArrays.js
@@ -4,18 +4,20 @@ const educations = [
 		Name: "HBO - Front-End Developer",
 		Location: "WINC Academy | 2020 - 2021",
 		Definition:
-			"Omscholing tot Front-End Developer. Behandelde onderwerpen: HTML, CSS/SCSS, Programmeren met Flexbox en Grid, Programmeren op de BEM & Sass manier, (vanilla) Javascript, Git & Github, Data ophalen via Postman en Javascript, TDD met Jest, React."
+			"Omscholing tot Front-End Developer. Behandelde onderwerpen:  HTML, CSS/SCSS, Programmeren met Flexbox en Grid, Programmeren op de BEM & Sass manier, (vanilla) Javascript, Git & Github, Data ophalen via Postman en Javascript, TDD met Jest en Programeren met React."
 	},
 	{
 		Id: 02,
 		Name: "BBL niveau 2 - Groenvoorziening",
 		Location: "Groenhorst College Emmeloord | 2000 - 2002",
-		Definition: null
+		Definition:
+			"Hoveniersopleiding voor groot groen Behandelde onderwerpen: Plantkunde, Beplanting en onderhoudt, Werken met kleine machines als kettingzaag, bosmaaier met slagblad & draad en Machine onderhoudt"
 	},
 	{
 		Id: 03,
 		Name: "VMBO niveau 2",
 		Location: "Groenhorst College Emmeloord | 1996 - 2000",
-		Definition: null
+		Definition:
+			"Middelbare onderwijs met eindvakken: Nederlands, Engels, Biologie, Wiskunde, Plantkunde en Techniek"
 	}
 ];
