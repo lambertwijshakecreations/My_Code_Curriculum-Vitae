c2487965d661faacfdc49a73b51f0c31bc7e5375
lambertwijshakecreations <wijshake_zwf@hotmail.com>
2021-11-15 17:30:25 +0100

    Add new experiance

--- Javascript/experience.js
+++ Javascript/experience.js
@@ -7,52 +7,72 @@ const myWorkExperience = () => {
 	newH1.innerText = "Werkervaring";
 	experienceContainer.appendChild(newH1);
 
-	//Container1 Bureau Techniek
+	//Container1 Arbeidsmakelaar Van der Steege
 	const newH3Con1 = document.createElement("h3");
-	newH3Con1.innerText = "Procesoperator";
+	newH3Con1.innerText = "Procesoperator/CNC Glasverspanner";
 	experienceContainer.appendChild(newH3Con1);
 	const newH4Con1 = document.createElement("h4");
-	newH4Con1.innerText = "Uitzendwerk via Bureau Techniek | 2020 - 2021";
+	newH4Con1.innerText = "Werk via Arbeidmakelaar van der Steege | 2021 - Heden";
 	experienceContainer.appendChild(newH4Con1);
 	const newUlCon1 = document.createElement("ul");
 	const newLi1Con1 = document.createElement("li");
 	newLi1Con1.innerText =
-		"Als procesoperator werkzaam geweest bij Bergschenhoek Luchtcomfort in Staphorst";
+		"Met AutoCAD pas ik tekeningen aan zodat ze door de CNC machiene gelezen kunnen worden";
 	newUlCon1.appendChild(newLi1Con1);
 	const newLi2Con1 = document.createElement("li");
-	newLi2Con1.innerText =
-		"Werkzaam als Lasrobot Operator bij Jan Kuipers in Nunspeet";
+	newLi2Con1.innerText = "instellen van de CNC machines";
 	newUlCon1.appendChild(newLi2Con1);
+	const newLi3Con1 = document.createElement("li");
+	newLi3Con1.innerText = "Meehelpen in de productie";
+	newUlCon1.appendChild(newLi3Con1);
 	experienceContainer.appendChild(newUlCon1);
 
-	//Container2 Everbake Group
+	//Container2 Bureau Techniek
 	const newH3Con2 = document.createElement("h3");
 	newH3Con2.innerText = "Procesoperator";
 	experienceContainer.appendChild(newH3Con2);
 	const newH4Con2 = document.createElement("h4");
-	newH4Con2.innerText = "Everbake Group B.V. | 2006 - 2020";
+	newH4Con2.innerText = "Uitzendwerk via Bureau Techniek | 2020 - 2021";
 	experienceContainer.appendChild(newH4Con2);
 	const newUlCon2 = document.createElement("ul");
 	const newLi1Con2 = document.createElement("li");
 	newLi1Con2.innerText =
-		"Het instellen en programeren van besturingsprogramma's van ABB-robotarmen.";
+		"Als procesoperator werkzaam geweest bij Bergschenhoek Luchtcomfort in Staphorst";
 	newUlCon2.appendChild(newLi1Con2);
 	const newLi2Con2 = document.createElement("li");
 	newLi2Con2.innerText =
-		"Het onderhouden van deze robotics en machines eromheen.";
+		"Werkzaam als Lasrobot Operator bij Jan Kuipers in Nunspeet";
 	newUlCon2.appendChild(newLi2Con2);
-	const newLi3Con2 = document.createElement("li");
-	newLi3Con2.innerText =
-		"Verantwoordelijk voor het verwerken van orders op de bandlasmachine. Deze machine maakt door middel van weerstand-lasmachines en lasrobots transportbanden die met name in grote bakkerijen worden gebruikt.";
-	newUlCon2.appendChild(newLi3Con2);
-	const newLi4Con2 = document.createElement("li");
-	newLi4Con2.innerText = "Magazijnbeheer voor eigen voorraad.";
-	newUlCon2.appendChild(newLi4Con2);
-	const newLi5Con2 = document.createElement("li");
-	newLi5Con2.innerText =
-		"De afwerking van producten door middel van lassen, slijpen, ponsen en plaatwerk.";
-	newUlCon2.appendChild(newLi5Con2);
 	experienceContainer.appendChild(newUlCon2);
+
+	//Container3 Everbake Group
+	const newH3Con3 = document.createElement("h3");
+	newH3Con3.innerText = "Procesoperator";
+	experienceContainer.appendChild(newH3Con3);
+	const newH4Con3 = document.createElement("h4");
+	newH4Con3.innerText = "Everbake Group B.V. | 2006 - 2020";
+	experienceContainer.appendChild(newH4Con3);
+	const newUlCon3 = document.createElement("ul");
+	const newLi1Con3 = document.createElement("li");
+	newLi1Con3.innerText =
+		"Het instellen en programeren van besturingsprogramma's van ABB-robotarmen.";
+	newUlCon3.appendChild(newLi1Con3);
+	const newLi2Con3 = document.createElement("li");
+	newLi2Con3.innerText =
+		"Het onderhouden van deze robotics en machines eromheen.";
+	newUlCon3.appendChild(newLi2Con3);
+	const newLi3Con3 = document.createElement("li");
+	newLi3Con3.innerText =
+		"Verantwoordelijk voor het verwerken van orders op de bandlasmachine. Deze machine maakt door middel van weerstand-lasmachines en lasrobots transportbanden die met name in grote bakkerijen worden gebruikt.";
+	newUlCon3.appendChild(newLi3Con3);
+	const newLi4Con3 = document.createElement("li");
+	newLi4Con3.innerText = "Magazijnbeheer voor eigen voorraad.";
+	newUlCon3.appendChild(newLi4Con3);
+	const newLi5Con3 = document.createElement("li");
+	newLi5Con3.innerText =
+		"De afwerking van producten door middel van lassen, slijpen, ponsen en plaatwerk.";
+	newUlCon3.appendChild(newLi5Con3);
+	experienceContainer.appendChild(newUlCon3);
 	main.appendChild(experienceContainer);
 };
 
